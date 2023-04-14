2892727e0a4e71b445503b603916417034b3c56e
TungstenVn <huutung2003@gmail.com>
2023-04-14 19:27:28 +0700

    update Client's Area

---
+++ TiemChungThuCung/Areas/Client/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiemChungThuCung.Areas.Client.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Client/Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+    }
+}
